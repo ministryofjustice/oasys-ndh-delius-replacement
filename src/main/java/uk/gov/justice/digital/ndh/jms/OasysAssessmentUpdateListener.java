982b774e03b15ffc3123f1d2da0d00a5540305c4
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-07-18 11:33:56 +0100

    Don't forget the listener.

---
+++ src/main/java/uk/gov/justice/digital/ndh/jms/OasysAssessmentUpdateListener.java
@@ -0,0 +1,83 @@
+package uk.gov.justice.digital.ndh.jms;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import uk.gov.justice.digital.ndh.api.delius.request.DeliusAssessmentUpdateSoapEnvelope;
+import uk.gov.justice.digital.ndh.api.delius.response.DeliusResponse;
+import uk.gov.justice.digital.ndh.api.oasys.request.NdhAssessmentUpdateSoapEnvelope;
+import uk.gov.justice.digital.ndh.service.OasysAssessmentService;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static uk.gov.justice.digital.ndh.config.JmsConfig.OASYS_MESSAGES;
+
+@Component
+@Slf4j
+public class OasysAssessmentUpdateListener {
+
+    private final XmlMapper xmlMapper;
+    private final Function<Optional<DeliusResponse>, Boolean> shouldAcknowledge = Optional::isPresent;
+    private final OasysAssessmentService oasysAssessmentService;
+    private final Function<Optional<DeliusResponse>, Optional<DeliusResponse>> faultHandler;
+
+    @Autowired
+    public OasysAssessmentUpdateListener(XmlMapper xmlMapper, OasysAssessmentService oasysAssessmentService) {
+        this.xmlMapper = xmlMapper;
+        this.oasysAssessmentService = oasysAssessmentService;
+
+        faultHandler = maybeDeliusResponse -> {
+            maybeDeliusResponse.ifPresent(deliusResponse -> {
+                if (deliusResponse.isFault()) {
+                    try {
+                        oasysAssessmentService.publishFault(deliusResponse);
+                    } catch (JsonProcessingException e) {
+                        log.error(e.getMessage());
+                    }
+                }
+            });
+
+            return maybeDeliusResponse;
+        };
+    }
+
+    @JmsListener(destination = OASYS_MESSAGES, concurrency = "1")
+    public void onMessage(Message message) {
+
+        val maybeUpdate = readMessage(message);
+
+        //TODO: Call the transformer, but in the meantime just hard wire
+        val maybeTransformed = maybeUpdate.map(update -> DeliusAssessmentUpdateSoapEnvelope.builder().build());
+
+        final val maybeResult = maybeTransformed.flatMap(oasysAssessmentService::deliusWebServiceResponseOf);
+
+        boolean doJmsAck = faultHandler.andThen(shouldAcknowledge).apply(maybeResult);
+
+        log.info("Acknowledging message {} : {}", message, doJmsAck);
+
+        if (doJmsAck) {
+            try {
+                message.acknowledge();
+            } catch (JMSException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
+    private Optional<NdhAssessmentUpdateSoapEnvelope> readMessage(Message message) {
+        try {
+            return Optional.ofNullable(xmlMapper.readValue(((TextMessage) message).getText(), NdhAssessmentUpdateSoapEnvelope.class));
+        } catch (Exception e) {
+            log.error("Can't turn message {0} into NdhAssessmentUpdateSoapEnvelope.", message);
+        }
+        return Optional.empty();
+    }
+}
