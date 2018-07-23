15443e337e50895fafece6a09367fc464d48e476
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-07-23 12:02:47 +0100

    Lombokify

--- src/main/java/uk/gov/justice/digital/ndh/jpa/entity/MsgStore.java
+++ src/main/java/uk/gov/justice/digital/ndh/jpa/entity/MsgStore.java
@@ -1,180 +1,46 @@
 package uk.gov.justice.digital.ndh.jpa.entity;
 
-import javax.persistence.*;
-import java.sql.Timestamp;
-import java.util.Objects;
+import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Data
 @Entity
-@Table(name = "MSG_STORE", schema = "NDH", catalog = "")
+@Table(name = "MSG_STORE")
 @IdClass(MsgStorePK.class)
 public class MsgStore {
-    private Integer msgStoreSeq;
-    private String correlationId;
-    private String customId;
-    private Timestamp msgTimestamp;
-    private Timestamp storeDatetime;
-    private String processName;
-    private String msgProcState;
-    private String msgBadFlag;
-    private String msgRecoverFlag;
-    private String msgDeliveredFlag;
-    private String payloadZipFlag;
-    private String payloadContentType;
-    private String payload;
-
     @Id
     @Column(name = "MSG_STORE_SEQ")
-    public Integer getMsgStoreSeq() {
-        return msgStoreSeq;
-    }
-
-    public void setMsgStoreSeq(Integer msgStoreSeq) {
-        this.msgStoreSeq = msgStoreSeq;
-    }
-
-    @Basic
-    @Column(name = "CORRELATION_ID")
-    public String getCorrelationId() {
-        return correlationId;
-    }
-
-    public void setCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
-    }
-
-    @Basic
-    @Column(name = "CUSTOM_ID")
-    public String getCustomId() {
-        return customId;
-    }
-
-    public void setCustomId(String customId) {
-        this.customId = customId;
-    }
-
-    @Basic
-    @Column(name = "MSG_TIMESTAMP")
-    public Timestamp getMsgTimestamp() {
-        return msgTimestamp;
-    }
-
-    public void setMsgTimestamp(Timestamp msgTimestamp) {
-        this.msgTimestamp = msgTimestamp;
-    }
-
+    private Integer msgStoreSeq;
     @Id
     @Column(name = "STORE_DATETIME")
-    public Timestamp getStoreDatetime() {
-        return storeDatetime;
-    }
-
-    public void setStoreDatetime(Timestamp storeDatetime) {
-        this.storeDatetime = storeDatetime;
-    }
-
-    @Basic
+    private Timestamp storeDatetime;
+    @Column(name = "CORRELATION_ID")
+    private String correlationId;
+    @Column(name = "CUSTOM_ID")
+    private String customId;
+    @Column(name = "MSG_TIMESTAMP")
+    private Timestamp msgTimestamp;
     @Column(name = "PROCESS_NAME")
-    public String getProcessName() {
-        return processName;
-    }
-
-    public void setProcessName(String processName) {
-        this.processName = processName;
-    }
-
-    @Basic
+    private String processName;
     @Column(name = "MSG_PROC_STATE")
-    public String getMsgProcState() {
-        return msgProcState;
-    }
-
-    public void setMsgProcState(String msgProcState) {
-        this.msgProcState = msgProcState;
-    }
-
-    @Basic
+    private String msgProcState;
     @Column(name = "MSG_BAD_FLAG")
-    public String getMsgBadFlag() {
-        return msgBadFlag;
-    }
-
-    public void setMsgBadFlag(String msgBadFlag) {
-        this.msgBadFlag = msgBadFlag;
-    }
-
-    @Basic
+    private String msgBadFlag;
     @Column(name = "MSG_RECOVER_FLAG")
-    public String getMsgRecoverFlag() {
-        return msgRecoverFlag;
-    }
-
-    public void setMsgRecoverFlag(String msgRecoverFlag) {
-        this.msgRecoverFlag = msgRecoverFlag;
-    }
-
-    @Basic
+    private String msgRecoverFlag;
     @Column(name = "MSG_DELIVERED_FLAG")
-    public String getMsgDeliveredFlag() {
-        return msgDeliveredFlag;
-    }
-
-    public void setMsgDeliveredFlag(String msgDeliveredFlag) {
-        this.msgDeliveredFlag = msgDeliveredFlag;
-    }
-
-    @Basic
+    private String msgDeliveredFlag;
     @Column(name = "PAYLOAD_ZIP_FLAG")
-    public String getPayloadZipFlag() {
-        return payloadZipFlag;
-    }
-
-    public void setPayloadZipFlag(String payloadZipFlag) {
-        this.payloadZipFlag = payloadZipFlag;
-    }
-
-    @Basic
+    private String payloadZipFlag;
     @Column(name = "PAYLOAD_CONTENT_TYPE")
-    public String getPayloadContentType() {
-        return payloadContentType;
-    }
-
-    public void setPayloadContentType(String payloadContentType) {
-        this.payloadContentType = payloadContentType;
-    }
-
-    @Basic
+    private String payloadContentType;
     @Column(name = "PAYLOAD")
-    public String getPayload() {
-        return payload;
-    }
+    private String payload;
 
-    public void setPayload(String payload) {
-        this.payload = payload;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MsgStore msgStore = (MsgStore) o;
-        return Objects.equals(msgStoreSeq, msgStore.msgStoreSeq) &&
-                Objects.equals(correlationId, msgStore.correlationId) &&
-                Objects.equals(customId, msgStore.customId) &&
-                Objects.equals(msgTimestamp, msgStore.msgTimestamp) &&
-                Objects.equals(storeDatetime, msgStore.storeDatetime) &&
-                Objects.equals(processName, msgStore.processName) &&
-                Objects.equals(msgProcState, msgStore.msgProcState) &&
-                Objects.equals(msgBadFlag, msgStore.msgBadFlag) &&
-                Objects.equals(msgRecoverFlag, msgStore.msgRecoverFlag) &&
-                Objects.equals(msgDeliveredFlag, msgStore.msgDeliveredFlag) &&
-                Objects.equals(payloadZipFlag, msgStore.payloadZipFlag) &&
-                Objects.equals(payloadContentType, msgStore.payloadContentType) &&
-                Objects.equals(payload, msgStore.payload);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(msgStoreSeq, correlationId, customId, msgTimestamp, storeDatetime, processName, msgProcState, msgBadFlag, msgRecoverFlag, msgDeliveredFlag, payloadZipFlag, payloadContentType, payload);
-    }
 }
--- src/main/java/uk/gov/justice/digital/ndh/jpa/entity/MsgStorePK.java
+++ src/main/java/uk/gov/justice/digital/ndh/jpa/entity/MsgStorePK.java
@@ -1,47 +1,19 @@
 package uk.gov.justice.digital.ndh.jpa.entity;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Objects;
 
+@Data
 public class MsgStorePK implements Serializable {
-    private Integer msgStoreSeq;
-    private Timestamp storeDatetime;
-
     @Column(name = "MSG_STORE_SEQ")
     @Id
-    public Integer getMsgStoreSeq() {
-        return msgStoreSeq;
-    }
-
-    public void setMsgStoreSeq(Integer msgStoreSeq) {
-        this.msgStoreSeq = msgStoreSeq;
-    }
-
+    private Integer msgStoreSeq;
     @Column(name = "STORE_DATETIME")
     @Id
-    public Timestamp getStoreDatetime() {
-        return storeDatetime;
-    }
+    private Timestamp storeDatetime;
 
-    public void setStoreDatetime(Timestamp storeDatetime) {
-        this.storeDatetime = storeDatetime;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MsgStorePK that = (MsgStorePK) o;
-        return Objects.equals(msgStoreSeq, that.msgStoreSeq) &&
-                Objects.equals(storeDatetime, that.storeDatetime);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(msgStoreSeq, storeDatetime);
-    }
 }
