1ebfd96a424a9ca9ef78d0ddef4ecb8c9a0c4e17
AntonyGowland <tonyuk28@googlemail.com>
2018-12-20 10:10:57 +0000

    added riskUpdate XML examples

--- src/main/java/uk/gov/justice/digital/ndh/service/XtagTransformer.java
+++ src/main/java/uk/gov/justice/digital/ndh/service/XtagTransformer.java
@@ -487,6 +487,7 @@ public class XtagTransformer {
                 .sentenceDays(daysOf(maybeSentenceCalculation.map(SentenceCalculation::getEffectiveSentenceLength).orElse(null)))
                 .sentenceDate(sentenceStartDateOf(activeSentences))
                 .releaseDate(maybeSentenceCalculation.map(sc -> sc.getReleaseDate().toString()).orElse(null))
+                .prisonNumber(inmateDetail.getBookingNo())
                 .pnc(pncOf(offender))
                 .nomisId(offender.getNomsId())
                 .forename1(offender.getFirstName())
