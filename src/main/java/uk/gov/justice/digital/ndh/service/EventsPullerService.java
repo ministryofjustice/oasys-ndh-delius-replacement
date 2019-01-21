67c28ac0ea07dfd4ade7d4a91afe19b79cbd592c
mikejackson <michael.jackson@digital.justice.gov.uk>
2019-01-21 15:09:55 +0000

    Guard against npe

--- src/main/java/uk/gov/justice/digital/ndh/service/EventsPullerService.java
+++ src/main/java/uk/gov/justice/digital/ndh/service/EventsPullerService.java
@@ -107,7 +107,7 @@ public class EventsPullerService {
                 log.info("No events to pull...");
             }
         } catch (Exception e) {
-            log.error("Caught error in processing loop: message is {}, class {}", e.getMessage(), e.getClass());
+            log.error("Caught error in processing loop: message is {}, class {}, stack trace {} ", e.getMessage(), e.getClass(), e.getStackTrace());
         }
     }
 
