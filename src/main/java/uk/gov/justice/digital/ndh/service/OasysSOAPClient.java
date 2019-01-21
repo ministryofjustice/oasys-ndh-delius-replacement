c00602b3388fd7127dc7bc3e49acb9d3f0527101
mikejackson <michael.jackson@digital.justice.gov.uk>
2019-01-21 10:44:55 +0000

    Add logging

--- src/main/java/uk/gov/justice/digital/ndh/service/OasysSOAPClient.java
+++ src/main/java/uk/gov/justice/digital/ndh/service/OasysSOAPClient.java
@@ -3,10 +3,15 @@ package uk.gov.justice.digital.ndh.service;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.body.RequestBodyEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.justice.digital.ndh.service.transforms.CommonTransformer;
 
 @Component
+@Slf4j
 public class OasysSOAPClient {
 
     private final String oasysUrl;
@@ -14,25 +19,36 @@ public class OasysSOAPClient {
     private final String oasysPassword;
     private final String hostName;
     private final String soapAction;
+    private final CommonTransformer commonTransformer;
 
+    @Autowired
     public OasysSOAPClient(@Value("${oasys.xtag.url}") String oasysUrl,
                            @Value(("${oasys.xtag.user:nouser}")) String oasysUser,
                            @Value("${oasys.xtag.password:nopassword}") String oasysPassword,
                            @Value("${oasys.xtag.hostname:nohostname}") String hostName,
-                           @Value("${oasys.xtag.soapaction:nosoapaction}") String soapAction) {
+                           @Value("${oasys.xtag.soapaction:nosoapaction}") String soapAction,
+                           final CommonTransformer commonTransformer) {
         this.oasysUrl = oasysUrl;
         this.oasysUser = oasysUser;
         this.oasysPassword = oasysPassword;
         this.hostName = hostName;
         this.soapAction = soapAction;
+        this.commonTransformer = commonTransformer;
     }
 
     public HttpResponse<String> oasysWebServiceResponseOf(String oasysSoapXml) throws UnirestException {
-        return Unirest.post(oasysUrl)
+
+        log.info("Posting to {} with body beginning {}...", oasysUrl, commonTransformer.limitLength(oasysSoapXml,50));
+        final RequestBodyEntity body = Unirest.post(oasysUrl)
                 .basicAuth(oasysUser, oasysPassword)
                 .header("Host", hostName)
                 .header("SOAPAction", "//" + soapAction)
                 .header("Content-Type", "application/soap+xml")
-                .body(oasysSoapXml).asString();
+                .body(oasysSoapXml);
+
+        final HttpResponse<String> stringHttpResponse = body.asString();
+        log.info("... with response status {}", stringHttpResponse.getStatus());
+
+        return stringHttpResponse;
     }
 }
